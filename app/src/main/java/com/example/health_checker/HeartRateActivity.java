c3cfe861a1ec3b74a909bc1191f13fceb86a7b40
Parshwa Shah <parshwa1999@gmail.com>
2021-02-10 16:44:45 -0700

    v1

---
+++ app/src/main/java/com/example/health_checker/HeartRateActivity.java
@@ -0,0 +1,14 @@
+package com.example.health_checker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeartRateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart_rate);
+    }
+}
--- app/src/main/java/com/example/health_checker/MainActivity.java
+++ app/src/main/java/com/example/health_checker/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.health_checker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,19 +14,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //val MeasureRespiratoryRateButton = findViewById(R.id.MeasureRespiratoryButton)
+        //Call Respiratory Rate Activity
         Button RespBtn = (Button) findViewById(R.id.MeasureRespiratoryButton);
-        Button HeartRateBtn = (Button) findViewById(R.id.MeasureHeartRateButton);
-        Button SymptomsBtn = (Button) findViewById(R.id.ReportSymptomsButton);
 
         RespBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent respiInt = new Intent(MainActivity.this, RespiratoryRateActivity.class);
+                startActivity(respiInt);
             }
         });
 
-                
-                
+        //Call Heart Rate Activity
+        Button HeartBtn = (Button) findViewById(R.id.MeasureHeartRateButton);
+
+        HeartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent heartInt = new Intent(MainActivity.this, HeartRateActivity.class);
+                startActivity(heartInt);
+            }
+        });
+
+        //Call Symptom Logging Activity
+        Button SymBtn = (Button) findViewById(R.id.ReportSymptomsButton);
+
+        SymBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent symInt = new Intent(MainActivity.this, SymptomLoggingActivity.class);
+                startActivity(symInt);
+            }
+        });
+
     }
 }
