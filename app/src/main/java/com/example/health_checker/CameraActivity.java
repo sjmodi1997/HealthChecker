63af10dd01366e7ab8b91bdabb9780d731c8452a
sjmodi1997 <sjmodi1997.sm@gmail.com>
2021-09-23 13:48:12 -0700

    Adding more Logs

--- app/src/main/java/com/example/health_checker/CameraActivity.java
+++ app/src/main/java/com/example/health_checker/CameraActivity.java
@@ -21,10 +21,12 @@ public class CameraActivity {
     // Function to measure the heart rate
     public String measure_heart_rate(String videoPath, String videoName) throws IOException {
         VideoCapture videoCapture = new VideoCapture();
-
+        Log.i("TAG", "Path :: " + videoPath+videoName);
         if (new File(videoPath + videoName).exists()) {
+            Log.i("TAG","File found!");
             videoCapture.open(videoPath + videoName);
             if (videoCapture.isOpened()) {
+                Log.i("TAG","Video Opened!");
                 List<Double> extremes = new ArrayList<Double>();
                 List<Double> list = new ArrayList<Double>();
                 List<Double> new_list = new ArrayList<Double>();
@@ -35,6 +37,7 @@ public class CameraActivity {
                 int video_length = (int) videoCapture.get(Videoio.CAP_PROP_FRAME_COUNT);
                 int fps = (int) videoCapture.get(Videoio.CAP_PROP_FPS);
 
+                Log.i("TAG", "Processing Heart-Rate :: 25%...");
                 videoCapture.read(current_frame);
                 for (int k = 0; k < video_length - 1; k++) {
                     videoCapture.read(next_frame);
@@ -42,7 +45,7 @@ public class CameraActivity {
                     next_frame.copyTo(current_frame);
                     list.add(Core.mean(diff_frame).val[0] + Core.mean(diff_frame).val[1] + Core.mean(diff_frame).val[2]);
                 }
-
+                Log.i("TAG", "Processing Heart-Rate :: 50%...");
                 for (int i = 0; i < (list.size() / 5) - 1; i++) {
                     List<Double> sublist = list.subList(i * 5, (i + 1) * 5);
                     double sum = 0.0;
@@ -56,7 +59,12 @@ public class CameraActivity {
                 int mov_period = 50;
 
                 List<Double> avg_data = algorithms.calcMovingAvg(mov_period, new_list);
+
+                Log.i("TAG", "Processing Heart-Rate :: 75%...");
+
                 int peakCounts = algorithms.countZeroCrossings(avg_data);
+
+                Log.i("TAG", "Processing Heart-Rate :: 99%...");
 
                 double fpsSec = (video_length / fps);
                 double count_heart_rate = (peakCounts / 2) * (60) / fpsSec;
@@ -64,10 +72,11 @@ public class CameraActivity {
                 return "" + (int) count_heart_rate;
 
             } else {
+                Log.i("TAG", "Not able to open file!");
                 return "";
             }
         } else {
-
+            Log.i("TAG", "File Not Found");
             return "";
         }
     }
